c14724cc739703f7ccf797c07d33a3f7e0c9a68f
Amit Dhakre <amitdhakre13@gmail.com>
2020-10-10 21:39:39 +0530

    Creating a Service to Get Album Data

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import map from 'rxjs/add/operator';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+  private _albumUrl = '../assets/album.json';
+
+  getAlbum(id: number) : any  {
+    return this._http.get(this._albumUrl); 
+  }
+
+}
